3721da3eeb72106715d2bdab69997c1242ca2f01
Tung N Le <tung.le0319@gmail.com>
2022-11-20 15:09:44 -0700

    tung

--- goodReads.client/src/pages/DetailsPage.vue
+++ goodReads.client/src/pages/DetailsPage.vue
@@ -1,15 +1,35 @@
 <template>
- <div class="container" v-if="book">
+ <div class="container mt-4" v-if="book">
    <div class="row bookRow">
   <div class="col-md-3">
-   <img :src="book.img" alt="" class="img-fluid">
+   <img :src="book.img" alt="" class="img-fluid" width="400" height="600">
 
   </div>
   <div class="col-md-6">
-<h1>{{book.title}}</h1>
+<h1 class="bookTitle">{{book.title}}</h1>
 <p class="authors" v-for="a in book.author">{{a}} </p>
   </div>
-  <div class="col-md-3"></div>
+  <div class="col-md-3">
+    <div class="card p-2">
+      <p>2.99</p>
+      <p>Available for a limited time</p>
+      <button class="btn btn-primary">Get Deal</button>
+      <div class="card-body d-flex flex-column justify-content-center">
+        <button class="btn btn-outline-dark"> Save To WishList</button>
+        <button class="btn btn-outline-dark mt-2"> Review</button>
+        <div>
+          <small>Share with your network</small>
+          <div>
+            <i class="mdi mdi-facebook fs-2"></i>
+            <i class="mdi mdi-twitter fs-2"></i>
+            <i class="mdi mdi-pinterest fs-2"></i>
+            <i class="mdi mdi-mail fs-2"></i>
+            <i class="mdi mdi-link fs-2"></i>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
    </div>
  </div>
 </template>
@@ -50,5 +70,8 @@ export default {
 <style scoped lang="scss">
 .bookRow{
   height: 100vh;
+}
+.bookTitle{
+  font-size: 40pt;
 }
 </style>
