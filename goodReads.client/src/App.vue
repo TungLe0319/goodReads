098eca666c341c68c1d9df31a633171d25f909d6
Tri Le <letrin93@gmail.com>
2022-11-20 16:42:49 -0700

    tri

--- goodReads.client/src/App.vue
+++ goodReads.client/src/App.vue
@@ -33,7 +33,7 @@ export default {
 
 
 main{
-  // background-image: url(https://images.unsplash.com/photo-1447023029226-ef8f6b52e3ea?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1771&q=80);
+  background-image: url(https://images.unsplash.com/photo-1457369804613-52c61a468e7d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80);
 
   background-attachment: fixed;
   background-size: cover;
