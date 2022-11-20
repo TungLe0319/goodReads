dd29c00a799345380b34583801bff53596d2cfd6
TungLe0319 <tung.le0319@gmail.com>
2022-11-20 13:33:04 -0700

    tung

--- goodReads.client/src/env.js
+++ goodReads.client/src/env.js
@@ -1,6 +1,6 @@
-export const dev = window.location.origin.includes('localhost')
-export const baseURL = dev ? 'http://localhost:3000' : ''
-export const useSockets = false
-export const domain = ''
-export const clientId = ''
-export const audience = ''
+export const dev = window.location.origin.includes('localhost');
+export const baseURL = dev ? 'https://localhost:7045' : '';
+export const useSockets = false;
+export const domain = 'bcwtungle.us.auth0.com';
+export const audience = 'https://bcw-cocobeans.com';
+export const clientId = 'cH9nhmj9T9447D5GUi8eTVHtBhxEg67x';
