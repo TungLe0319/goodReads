8359bc3ff6c304e015365667aeb85a170409c12f
daniel le <daniel97.le@gmail.com>
2022-11-23 13:21:34 -0700

    commit

--- goodReads/Models/BookShelf.cs
+++ goodReads/Models/BookShelf.cs
@@ -1,6 +1,7 @@
 namespace goodReads.Models;
 public class BookShelf : IHasCreator
 {
+  
   public string Type { get; set; }
   public string CreatorId { get; set; }
   public Profile Creator { get; set; }
@@ -8,5 +9,15 @@ public class BookShelf : IHasCreator
   public DateTime CreatedAt { get; set; }
   public DateTime UpdatedAt { get; set; }
 
+  
+
+}
+public class Shelf : BookShelf
+{
+  public Shelf(string type, string creatorId)
+  {
+    Type = type;
+    CreatorId = creatorId;
+  }
 }
 
