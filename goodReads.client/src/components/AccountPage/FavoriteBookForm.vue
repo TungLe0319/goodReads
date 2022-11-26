98b9a8e88610286998bde49f64097022e937068a
TungLe0319 <tung.le0319@gmail.com>
2022-11-25 22:39:15 -0700

    tung

--- goodReads.client/src/components/AccountPage/FavoriteBookForm.vue
+++ goodReads.client/src/components/AccountPage/FavoriteBookForm.vue
@@ -1,24 +1,36 @@
 <template>
-<div class="modal-body ">
+  <div class="modal-body">
+    <div class="row">
+      <div class="col-md-12 d-flex justify-content-center">
+        <h1>Search Books</h1>
+      </div>
+      <div class="col-md-12 d-flex justify-content-center">
+        <form @submit.prevent="addFavoriteBook()" class="d-flex flex-column">
+          <div class="my-5 position-relative">
+            <input
+              v-model="editable.title"
+              type="search"
+              name=""
+              id=""
+              class="p-2 form-control"
+            />
+           
+            <div class="position-absolute">
+              <ul class="list-group">
+                <li class="list-group-item" v-for="t in title">
+                  {{ t }}
+                </li>
+              </ul>
+            </div>
+          </div>
 
-  <div class="row">
-    <div class="col-md-12 d-flex justify-content-center">
-      <h1>Search Books</h1>
-
-    </div>
-    <div class="col-md-12 d-flex justify-content-center">
-      <form @submit.prevent="addFavoriteBook()" class="d-flex flex-column">
-        <div class="my-5">
-
-          <input v-model="editable.title" type="search" name="" id="" class="p-2 form-control">
-        </div>
-    
-    
-        <button type="submit" class="btn btn-primary mt-3">Submit Favorite</button>
-      </form>
+          <button type="submit" class="btn btn-primary mt-3">
+            Submit Favorite
+          </button>
+        </form>
+      </div>
     </div>
   </div>
-</div>
 </template>
 
 <script>
@@ -27,26 +39,20 @@ import { onMounted, ref, watchEffect } from "vue";
 import { AppState } from "../../AppState.js";
 import Pop from "../../utils/Pop.js";
 
-
 export default {
-props:{
-
-       },
+  props: {},
   setup(props) {
     const editable = ref({});
-    
-    onMounted(() => {
 
-    });
+    onMounted(() => {});
     watchEffect(() => {});
 
     return {
       editable,
-      title: computed(() => AppState.books.map(b=> b.title)),
-      }
-    }
-  }
+      title: computed(() => AppState?.books.map((b) => b.title).filter(x=> x.includes(editable.value.title))),
+    };
+  },
+};
 </script>
 
-<style lang="scss" scoped>
-</style>
+<style lang="scss" scoped></style>
