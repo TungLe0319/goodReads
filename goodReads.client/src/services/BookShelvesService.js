f25e5ffa9280c91c8c7b65083c3549da993d909a
daniel le <daniel97.le@gmail.com>
2022-11-26 14:46:40 -0700

    dan

--- goodReads.client/src/services/BookShelvesService.js
+++ goodReads.client/src/services/BookShelvesService.js
@@ -2,10 +2,6 @@ import { AppState } from "../AppState.js";
 import { api } from "./AxiosService.js";
 
 class BookShelvesService{
-
-
-
-
   async addToBookShelf(data){
 console.log(data);
     const res = await api.post('api/shelvedbooks',data)
