dfb3b0b11572489f5f0906d34724387fc2261ccb
Tung N Le <tung.le0319@gmail.com>
2022-11-27 12:19:37 -0700

    tung

--- goodReads.client/src/components/BookPage/AddToBookShelf.vue
+++ goodReads.client/src/components/BookPage/AddToBookShelf.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="card p-2 elevation-4 border-0">
-    <h3 v-if="book.saleAbility == 'Unavailable'" class="ms-3 text-danger text-decoration-underline">
+    <h3
+      v-if="book.saleAbility == 'Unavailable'"
+      class="ms-3 text-danger text-decoration-underline"
+    >
       {{ book.saleAbility }}
     </h3>
     <h3 v-else>{{ book.saleAbility }}</h3>
@@ -10,11 +13,14 @@
       <form @submit.prevent="addToBookShelf()">
         <div class="mb-3">
           <label for="" class="form-label">Add to BookShelf</label>
-          <select v-model="editable.id" class="form-select form-select-lg" name="" id="">
+          <select
+            v-model="editable.id"
+            class="form-select form-select-lg"
+            name=""
+            id=""
+          >
             <option :value="b.id" v-for="b in bookShelves">
-              <div :class="b.hasActiveBook ? 'text-warning' : ''">
-                {{ b.type }}
-              </div>
+              {{ b.type }}
             </option>
           </select>
         </div>
@@ -23,33 +29,61 @@
         </button>
       </form>
 
-      <button class="btn btn-outline-dark mt-2" data-bs-toggle="modal" data-bs-target="#createReview">
+      <button
+        class="btn btn-outline-dark mt-2"
+        data-bs-toggle="modal"
+        data-bs-target="#createReview"
+      >
         Review
       </button>
       <div class="mt-3">
         <small>Share with your network :</small>
         <div class="d-flex gap-2 justify-content-center mt-2">
-          <a target="_blank"
+          <a
+            target="_blank"
             OnClick="window.open(this.href,'targetWindow','toolbar=no,location=0,status=no,menubar=no,scrollbars=yes,resizable=yes,width=600,height=250'); return false;"
-            href="https://www.twitter.com">
-            <img src="src\assets\img\ShareIcons\twitter.png" alt="" width="30" />
+            href="https://www.twitter.com"
+          >
+            <img
+              src="src\assets\img\ShareIcons\twitter.png"
+              alt=""
+              width="30"
+            />
           </a>
 
           <a href="mailto:">
             <img src="src\assets\img\ShareIcons\email.png" alt="" width="30" />
           </a>
-          <img class="link" src="src\assets\img\ShareIcons\link.png" alt="" width="30" @click="copyToClipBoard()" />
+          <img
+            class="link"
+            src="src\assets\img\ShareIcons\link.png"
+            alt=""
+            width="30"
+            @click="copyToClipBoard()"
+          />
 
-          <a target="_blank"
+          <a
+            target="_blank"
             OnClick="window.open(this.href,'targetWindow','toolbar=no,location=0,status=no,menubar=no,scrollbars=yes,resizable=yes,width=600,height=250'); return false;"
-            href="https://www.facebook.com">
-            <img src="src\assets\img\ShareIcons\facebook.png" alt="" width="30" />
+            href="https://www.facebook.com"
+          >
+            <img
+              src="src\assets\img\ShareIcons\facebook.png"
+              alt=""
+              width="30"
+            />
           </a>
 
-          <a target="_blank"
+          <a
+            target="_blank"
             OnClick="window.open(this.href,'targetWindow','toolbar=no,location=0,status=no,menubar=no,scrollbars=yes,resizable=yes,width=600,height=250'); return false;"
-            href="http://pinterest.com">
-            <img src="src\assets\img\ShareIcons\pinterest.png" alt="" width="30" />
+            href="http://pinterest.com"
+          >
+            <img
+              src="src\assets\img\ShareIcons\pinterest.png"
+              alt=""
+              width="30"
+            />
           </a>
         </div>
       </div>
@@ -69,15 +103,14 @@ export default {
   props: {},
   setup(props) {
     const editable = ref({});
-    onMounted(() => { });
-    watchEffect(() => { });
+    onMounted(() => {});
+    watchEffect(() => {});
     const route = useRoute();
     return {
       editable,
       book: computed(() => AppState.activeBook),
       bookShelves: computed(() => AppState.accountBookshelves),
       inBookShelf: computed(() => {
-
         // for (const shelf of AppState.accountBookshelves) {
         //   let shelved = AppState.accountShelvedBooks.filter(s => s.bookShelfId != shelf.id)
         //   for (const books of shelved) {
@@ -87,24 +120,41 @@ export default {
         //   }
         // }
       }),
+      alreadyShelved: computed(() => {
+        if (AppState.accountBookshelves && AppState.accountShelvedBooks) {
+          let found = AppState.accountShelvedBooks.find(
+            (x) =>
+              x.bookShelfId == editable.value.id &&
+              x.id == AppState.activeBook.id
+          );
+          return found;
+        }
+      }),
+
       copyToClipBoard() {
         navigator.clipboard.writeText(route.fullPath);
         Pop.toast(`Copied To ClipBoard`);
       },
       async addToBookShelf() {
         try {
-          // console.log(AppState.accountBookshelves);
-          let shelf = AppState.accountBookshelves.find(s => s.id == editable.value.id)
-          if (shelf.hasActiveBook) {
-            console.log('shelf has book');
+          // console.log('bookId',this.alreadyShelved.id,"bookShelfId",this.alreadyShelved.bookShelfId);
+          if (this.alreadyShelved) {
+            Pop.toast("already apart of this list");
             return
           }
+          // let shelf = AppState.accountBookshelves.find(
+          //   (s) => s.id == editable.value.id
+          // );
+          // if (shelf.hasActiveBook) {
+          //   console.log("shelf has book");
+          //   return;
+          // }
 
           let data = {
             bookId: AppState.activeBook.id,
             bookShelfId: editable.value.id,
           };
-          console.log(data);
+          // console.log(data);
           await bookShelvesService.addToBookShelf(data);
         } catch (error) {
           Pop.error(error, "[addToBookShelf]");
