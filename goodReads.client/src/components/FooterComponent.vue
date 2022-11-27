773bb94218ee8bededec6df0dcc25fa17359e5e4
Tung N Le <tung.le0319@gmail.com>
2022-11-27 09:02:24 -0700

    tung

--- goodReads.client/src/components/FooterComponent.vue
+++ goodReads.client/src/components/FooterComponent.vue
@@ -1,23 +1,19 @@
 <template>
-  <!-- Remove the container if you want to extend the Footer to full width. -->
 
-  <!-- Footer -->
   <footer
     class="text-center text-lg-start text-white mt-5"
     style="background-color: #1c2331"
   >
-    <!-- Section: Social media -->
+
     <section
       class="d-flex justify-content-between p-3"
       style="background-color: #6351ce"
     >
-      <!-- Left -->
+
       <div class="me-5 d-flex align-items-center">
         <span>Get connected with us on social networks:</span>
       </div>
-      <!-- Left -->
 
-      <!-- Right -->
       <div class="">
         <a href="" class="text-white me-4 ">
           <i class="mdi mdi-facebook fs-1"></i>
@@ -38,18 +34,18 @@
           <i class="mdi mdi-github fs-1"></i>
         </a>
       </div>
-      <!-- Right -->
-    </section>
-    <!-- Section: Social media -->
 
-    <!-- Section: Links  -->
+    </section>
+
+
+
     <section class="">
       <div class="container text-center text-md-start mt-5">
-        <!-- Grid row -->
+  
         <div class="row mt-3">
-          <!-- Grid column -->
+     
           <div class="col-md-3 col-lg-4 col-xl-3 mx-2 mb-4">
-            <!-- Content -->
+
             <h6 class="text-uppercase fw-bold">BetterAfterBooks</h6>
             <hr
               class="mb-4 mt-0 d-inline-block mx-auto"
@@ -59,11 +55,9 @@
               Thank you for taking the time and viewing this application, If you would like to see more of our work please check out our Github Profiles and Get in touch with us through LinkedIn
             </p>
           </div>
-          <!-- Grid column -->
-
-          <!-- Grid column -->
+  
           <div class="col-md-2 col-lg-2 col-xl-2 mb-4">
-          <!-- Links -->
+   
             <h6 class="text-uppercase fw-bold">Useful links</h6>
             <hr
               class="mb-4 mt-0 d-inline-block mx-2"
@@ -72,9 +66,7 @@
             <p>
               <a href="#/account" class="text-white">Your Account</a>
             </p>
-            <p>
-              <a href="#!" class="text-white">Become an Affiliate</a>
-            </p>
+       
             <p>
               <a href="#/account/lists" class="text-white">Your Lists</a>
             </p>
@@ -82,11 +74,9 @@
               <a href="#" class="text-white">Help</a>
             </p>
           </div>
-          <!-- Grid column -->
-
-          <!-- Grid column -->
+ 
           <div class="col-md-3 col-lg-2 col-xl-2 mx-2 mb-4">
-            <!-- Links -->
+
             <h6 class="text-uppercase fw-bold">Contact</h6>
             <hr
               class="mb-4 mt-0 d-inline-block mx-auto"
@@ -98,9 +88,7 @@
             <p><i class="fas fa-print mr-3"></i> + 01 234 567 89</p>
 
           </div>
-          <!-- Grid column -->
 
-          <!-- Grid column -->
           <div class="col-md-4 col-lg-3 col-xl-3 mx-2 mb-md-0 mb-4">
       
             <div>
@@ -111,20 +99,18 @@
               />
             </div>
           </div>
-          <!-- Grid column -->
+
         </div>
-        <!-- Grid row -->
+  
       </div>
     </section>
-    <!-- Section: Links  -->
 
-    <!-- Copyright -->
     <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2)">
   
     </div>
-    <!-- Copyright -->
+
   </footer>
-  <!-- Footer -->
+
 </template>
 
 <script>
