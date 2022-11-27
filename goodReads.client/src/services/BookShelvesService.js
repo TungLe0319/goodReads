4889cf69d84cefb877766d31b46858f8a3e52bd8
daniel le <daniel97.le@gmail.com>
2022-11-27 12:56:25 -0700

    commit

--- goodReads.client/src/services/BookShelvesService.js
+++ goodReads.client/src/services/BookShelvesService.js
@@ -16,19 +16,22 @@ class BookShelvesService {
     // addMany(AppState.accountShelvedBooks, res.data, new ShelvedBook)
   }
   findBook() {
+    debugger
     let bookId = AppState.activeBook.id;
     for (const shelf of AppState.accountBookshelves) {
+      let id = shelf.id
       let shelved = AppState.accountShelvedBooks.filter(
-        (s) => s.bookShelfId != shelf.id && s.id != bookId
+        (s) =>  s.id == bookId && s.bookShelfId == shelf.id
       );
       for (const books of shelved) {
         if (books.id == bookId) {
           shelf.hasActiveBook = true;
+          console.log(shelf);
         }
       }
     }
-    console.log(AppState.accountShelvedBooks, "books");
-    console.log(AppState.accountBookshelves, "shelves");
+    console.log(AppState.accountShelvedBooks.map(s => new Object({title: s.title, shelf:s.bookShelfId})), "books");
+    console.log(AppState.accountBookshelves.map(g=> g.hasActiveBook), "shelves");
   }
 }
 export const bookShelvesService = new BookShelvesService();
