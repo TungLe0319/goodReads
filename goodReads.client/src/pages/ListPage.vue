649c7f1e2ea7f837268c66ff77656e6d90e8bbd3
Tung N Le <tung.le0319@gmail.com>
2022-11-27 10:24:27 -0700

    tungtung

--- goodReads.client/src/pages/ListPage.vue
+++ goodReads.client/src/pages/ListPage.vue
@@ -2,10 +2,8 @@
   <div class="container">
     <div class="row my-5">
       <div class="col-md-12">
-        <div class="card elevation-5 border-0 p-4 ">
+        <div class="card elevation-5 border-0 p-4">
           <h1>You Haven't Added any Books to your Wishlist Yet</h1>
-
-
         </div>
       </div>
     </div>
@@ -22,69 +20,115 @@
       <div class="col-md-12">
         <nav>
           <div class="nav nav-tabs" id="nav-tab" role="tablist">
-            <button class="nav-link active" id="nav-wishlist-tab" data-bs-toggle="tab" data-bs-target="#nav-wishlist"
-              type="button" role="tab" aria-controls="nav-wishlist" aria-selected="true"
-              @click="setActiveShelf('wishlist')">
+            <button
+              class="nav-link active"
+              id="nav-wishlist-tab"
+              data-bs-toggle="tab"
+              data-bs-target="#nav-wishlist"
+              type="button"
+              role="tab"
+              aria-controls="nav-wishlist"
+              aria-selected="true"
+              @click="setActiveShelf('wishlist')"
+            >
               Wishlist
             </button>
-            <button class="nav-link" id="nav-owned-tab" data-bs-toggle="tab" data-bs-target="#nav-owned" type="button"
-              role="tab" aria-controls="nav-owned" aria-selected="false" @click="setActiveShelf('favorite')">
+            <button
+              class="nav-link"
+              id="nav-owned-tab"
+              data-bs-toggle="tab"
+              data-bs-target="#nav-owned"
+              type="button"
+              role="tab"
+              aria-controls="nav-owned"
+              aria-selected="false"
+              @click="setActiveShelf('favorite')"
+            >
               Favorites
             </button>
-            <button class="nav-link" id="nav-reading-tab" data-bs-toggle="tab" data-bs-target="#nav-reading"
-              type="button" role="tab" aria-controls="nav-reading" aria-selected="false"
-              @click="setActiveShelf('reading')">
+            <button
+              class="nav-link"
+              id="nav-reading-tab"
+              data-bs-toggle="tab"
+              data-bs-target="#nav-reading"
+              type="button"
+              role="tab"
+              aria-controls="nav-reading"
+              aria-selected="false"
+              @click="setActiveShelf('reading')"
+            >
               Reading
             </button>
-            <button class="nav-link" id="nav-finished-tab" data-bs-toggle="tab" data-bs-target="#nav-finished"
-              type="button" role="tab" aria-controls="nav-finished" aria-selected="false"
-              @click="setActiveShelf('finished')">
+            <button
+              class="nav-link"
+              id="nav-finished-tab"
+              data-bs-toggle="tab"
+              data-bs-target="#nav-finished"
+              type="button"
+              role="tab"
+              aria-controls="nav-finished"
+              aria-selected="false"
+              @click="setActiveShelf('finished')"
+            >
               Finished
             </button>
           </div>
         </nav>
 
-
         <div class="tab-content" id="nav-tabContent">
-          <div class="tab-pane fade show active" id="nav-wishlist" role="tabpanel" aria-labelledby="nav-wishlist-tab"
-            tabindex="0">
+          <div
+            class="tab-pane fade show active"
+            id="nav-wishlist"
+            role="tabpanel"
+            aria-labelledby="nav-wishlist-tab"
+            tabindex="0"
+          >
             <div class="row mt-3">
               <div class="col-md-2" v-for="f in filtered" :key="f.id">
                 <BookCard :book="f" />
               </div>
             </div>
-
-
-
           </div>
 
-          <div class="tab-pane fade" id="nav-owned" role="tabpanel" aria-labelledby="nav-owned-tab" tabindex="0">
+          <div
+            class="tab-pane fade"
+            id="nav-owned"
+            role="tabpanel"
+            aria-labelledby="nav-owned-tab"
+            tabindex="0"
+          >
             <div class="row mt-3">
-              <div class="col-md-2 " v-for="f in filtered" :key="f.id">
+              <div class="col-md-2" v-for="f in filtered" :key="f.id">
                 <BookCard :book="f" />
-                <button class="fs-5 text-danger btn p-0"> Remove</button>
+          
               </div>
             </div>
-
-
           </div>
-          <div class="tab-pane fade" id="nav-reading" role="tabpanel" aria-labelledby="nav-reading-tab" tabindex="0">
+          <div
+            class="tab-pane fade"
+            id="nav-reading"
+            role="tabpanel"
+            aria-labelledby="nav-reading-tab"
+            tabindex="0"
+          >
             <div class="row mt-3">
               <div class="col-md-2" v-for="f in filtered" :key="f.id">
                 <BookCard :book="f" />
               </div>
             </div>
-
-
           </div>
-          <div class="tab-pane fade" id="nav-finished" role="tabpanel" aria-labelledby="nav-finished-tab" tabindex="0">
+          <div
+            class="tab-pane fade"
+            id="nav-finished"
+            role="tabpanel"
+            aria-labelledby="nav-finished-tab"
+            tabindex="0"
+          >
             <div class="row mt-3">
               <div class="col-md-2" v-for="f in filtered" :key="f.id">
                 <BookCard :book="f" />
               </div>
             </div>
-
-
           </div>
         </div>
       </div>
@@ -105,24 +149,30 @@ export default {
   setup() {
     const editable = ref({});
     onMounted(() => {
-      setWishListDefault()
+      setWishListDefault();
     });
-async function setWishListDefault(){
-  if (AppState.accountBookshelves) {
-  let found =  AppState.accountBookshelves.find(a=> a.type == 'wishlist')
-AppState.activeBookShelf = found
-  }
-}
+    async function setWishListDefault() {
+      if (AppState.accountBookshelves) {
+        let found = AppState.accountBookshelves.find(
+          (a) => a.type == "wishlist"
+        );
+        AppState.activeBookShelf = found;
+      }
+    }
     return {
       editable,
       account: computed(() => AppState.account),
-      filtered: computed(() => AppState.accountShelvedBooks?.filter(x => x.bookShelfId == AppState.activeBookShelf?.id)),
+      filtered: computed(() =>
+        AppState.accountShelvedBooks?.filter(
+          (x) => x.bookShelfId == AppState.activeBookShelf?.id
+        )
+      ),
       bookShelves: computed(() => AppState.accountShelvedBooks),
       setActiveShelf(x) {
-        let found = AppState.accountBookshelves.find(a => a.type == x)
-        AppState.activeBookShelf = found
+        let found = AppState.accountBookshelves.find((a) => a.type == x);
+        AppState.activeBookShelf = found;
         console.log(AppState.activeBookShelf);
-      }
+      },
     };
   },
   components: { BookCard },
