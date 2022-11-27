261496b04c8ede8fa7c977cf92f286849c84254e
Tung N Le <tung.le0319@gmail.com>
2022-11-27 08:53:20 -0700

    tung

--- goodReads/SQLTables/FileUpload.sql
+++
@@ -1,8 +0,0 @@
--- Active: 1669006953911@@bobocat.mysql.database.azure.com@3306@books
-
-CREATE TABLE
-    IF NOT EXISTS files(
-        file_name VARCHAR(255) NOT NULL primary key COMMENT 'primary key',
-        file_mime VARCHAR(255) NOT NULL,
-        file_data longblob NOT NULL
-    ) default charset utf8 COMMENT '';
