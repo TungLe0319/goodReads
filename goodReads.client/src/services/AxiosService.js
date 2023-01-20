4dde3bbefefd7e52b7fe8bd375810a7f27e03dfd
daniel <daniel97.le@gmail.com>
2023-01-20 15:18:46 -0700

    1

--- goodReads.client/src/services/AxiosService.js
+++ goodReads.client/src/services/AxiosService.js
@@ -8,8 +8,7 @@ export const googleBookApi = Axios.create({
   baseURL: "https://www.googleapis.com/books/v1",
   params: { key: googleKey},
   timeout: 8000,
-});
-
+})
 export const SandBox = new Axios.create({
   baseURL: 'https://bcw-sandbox.herokuapp.com/',
   timeout: 8000
